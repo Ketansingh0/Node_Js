3d992e3fb2643608cd87e11cb8e87426a6ea5177
Ketansingh0 <ketansingh831856@gmail.com>
2025-04-18 20:13:58 +0530

    expressjs

--- 8.ExpressJs1/app.js
+++ 8.ExpressJs1/app.js
@@ -1,2 +1,15 @@
-console.log("hello world");
-console.log("first comit");
+const express = require('express');
+const bodyParser = requier('body-parser');
+const app = express();
+
+app.use((req, res, next) =>{
+  console.log("First Dummy Middleware", req.url, req.method);
+  next();
+});
+
+// app.use(bodyParser.urlencoded());
+
+const PORT = 3000;
+app.listen(PORT, () =>{
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
